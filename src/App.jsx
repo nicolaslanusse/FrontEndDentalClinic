bfe59e4651af61894d5a3144c23052447347cb46
Nicolas <nicolaslanusse4@gmail.com>
2023-05-14 19:28:42 +0200

    add header to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
 import React from 'react'
-
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/header/Header'
 function App () {
-  return <>
-  
-  </>
-}
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
 
+        {/* Footer */}
+      </BrowserRouter>
+    </>
+  )
+}
 export default App
