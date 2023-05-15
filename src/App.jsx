765568cf1241919bfdbaa6991e233ac5c7b361e2
Nicolas <nicolaslanusse4@gmail.com>
2023-05-15 09:18:53 +0200

    update App.jsx and fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import './App.css'
 import { BrowserRouter } from 'react-router-dom'
-import Header from './components/header/Header'
+import Header from './components/Header'
+
 function App () {
   return (
     <>
       <BrowserRouter>
         <Header />
-
         {/* Footer */}
       </BrowserRouter>
     </>
