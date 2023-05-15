349cd17137129c463b40ae273ddc4de6505915da
Nicolas <nicolaslanusse4@gmail.com>
2023-05-15 09:26:12 +0200

    add and update LandingPage.jsx

---
+++ src/containers/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
+import Information from '../components/Information'
+import Steps from '../components/Steps'
+
+const LandingPage = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Information />
+      <Steps />
+      <Footer />
+    </>
+  )
+}
+
+export default LandingPage
